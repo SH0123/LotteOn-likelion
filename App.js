e016931a8251cfa532d74797a7a8d3b5a584e2ff
Sanghyo <sanghyoboy@naver.com>
2020-10-24 22:16:19 +0900

    fontsize not changing

--- App.js
+++ App.js
@@ -3,6 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 
+
+if (Text.defaultProps == null) Text.defaultProps = {};
+Text.defaultProps.allowFontScaling = false;
+
+
 export default function App() {
   return (
     <NavigationContainer style={styles.container}>
